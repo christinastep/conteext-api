2ba49236b0460050b2a9304291579ac30c8f7858
christina <christina@datagif.fr>
2020-04-17 11:54:18 +0200

    naja

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import './App.css';
+// javais commence comme ca mais le truc de pierre etait completement different
 
-function App() {
+
+// import React from 'react';
+// import './App.css';
+
+// import AuthContext from './context/AuthContext';
+// import Box from './componets/Box';
+
+// function App() {
+//   return (
+//     <div className="App">
+//      <AuthContext.Provider value="{ isAuth:false}">
+//       <Box />
+//     </AuthContext.Provider>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useContext } from 'react'
+
+const CurrentRoute = React.createContext({ path: '/welcome' })
+const CurrentUser = React.createContext(undefined)
+const IsAuth = React.createContext(false)
+
+export default () =>
+{
+  let currentRoute = useContext(CurrentRoute)
+  let currentUser = useContext(CurrentUser)
+  let isAuth = useContext(IsAuth)
+
   return (
-    <div className="App">
-     
+    // !isAuth &&
+    // currentRoute.path === '/welcome' &&
+    // (currentUser
+    //   ? `Welcome back, ${currentUser.name}!`
+    //   : 'Welcome!'
+    // )
+    <div>
+      
+      <p>{isAuth ? 'true' : 'false'}</p>
+      <button onClick={!isAuth}>
+        {isAuth ? 'ON' : 'OFF'}
+      </button>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/componets/Box.jsx
@@ -0,0 +1,11 @@
+// src/StudentList.js
+import React from 'react';
+import AuthContext from '../context/AuthContext';
+export default () => (
+
+    <AuthContext.Consumer>
+    {
+      //  <p>{value}</p>
+    }
+    </AuthContext.Consumer>
+);
